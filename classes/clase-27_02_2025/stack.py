ba212de33c02e262f969fb00339ccf5451cf924b
tutosrive <tutosrivegamer@gmail.com>
2025-03-04 14:18:39 -0500

    Add first version final to 'stack.py' and your functions

--- classes/clase-27_02_2025/stack.py
+++ classes/clase-27_02_2025/stack.py
@@ -13,9 +13,9 @@ def isEmpty(top:int) -> bool:
 		bool: Booleano que indicará si la lista ESTÁ o NO vacía 
 	"""
 	isNull: bool = False # Por defecto es vacía
-	# Validar si el TOPE (cima de la PILA) es mayor a 0
+	# Validar si el TOPE (cima de la PILA) es igual a 0
 	# de ser así, la lista está vacía
-	if top > 0:
+	if top == 0:
 		isNull = not isNull # Negación lógica
 	return isNull
 
@@ -40,10 +40,65 @@ def isFull(stack: list[int], top: int) -> bool:
 		isFilled = not isFilled # Negación lógica
 	return isFilled
 
+def pop_reference(stack: list[int], top_reference: list[int], value_reference: list[int]) -> int:
+	"""Eliminar último elemento de la PILA (con valores por referencia, solo retorna 1 ó 0)
+
+	### PRECONDICIONES: {`E/S int stack[n]`, `E/S int top[1]`, `E/S int value_reference[None]`}
+	### POSCONDICIONES: 
+			Se retorna:
+				int: 0 => Lista vacía || 1 => Operaciones ejecutadas con éxito
+			NOTA: Es claro que `stack`, `top_reference` y `value_reference` son "paso por referencia" y no se retornan
+	Args:
+		stack (list[int]): PILA con la cual se trabajará
+		top_reference (list[int]): Tope (cima) de la PILA (se debe especificar como un `int` en posición 0)
+		value_reference (list[None]): Almacenará el valor que se eliminará ó None
+
+	Returns:
+		int: `0` => Lista vacía || `1` => Operaciones ejecutadas con éxito
+	"""
+	if not isEmpty(top_reference[0]):
+		# Agregar el valor que se "eliminará"
+		# Usando lis.pop() eliminará el último valor y lo retornará
+		value_reference[0] = stack[top_reference[0]]
+		# Reducir tope en 1
+		top_reference[0] -= 1
+		# Operaciones ejecutadas con éxito
+		return 1
+	# PILA vacía
+	return 0
+
+def pop_multi_return(stack: list[int], top: int) -> tuple:
+	"""Eliminar último elemento de la PILA (con múltiples retornos (tupla))
+
+	### PRECONDICIONES: {`E/S int stack[n]`, `E/S int top`}
+	### POSCONDICIONES: 
+			Se retorna:
+				tuple: (0, top: int, value = None): Lista vacía || (1, top: int, value: int): Operaciones ejecutadas con éxito
+
+	Args:
+		stack (list[int]): PILA con la cual se trabajará
+		top (int): Índice del tope (cima, último elemento) de la PILA
+
+	Returns:
+		tuple: (0, top: int, None) Cuando la lista está vacía || (1, top: int, value: int)
+	"""
+	if not isEmpty(top):
+		# Agregar el valor que se "eliminará"
+		# Guardar el valor del TOPE ACTUAL
+		value:int = stack[top]
+		# Reducir tope en 1
+		top -= 1
+		# Operaciones ejecutadas con éxito
+		return 1, top, value
+	# PILA vacía
+	return 0, top, None
+
 def push(stack: list[int], value: int, top: int) -> None:
-	"""Agregar un elemento a la PILA 
+	"""Agregar un elemento a la PILA
 
 	### PRECONDICIONES: {`E/S int stack[n]`, `E int value`, `E/S int top`}
+	### POSCONDICIONES: 
+		La PILA deberá contener el nuevo "valor" pasado por parámetro y el TOPE (Cima) estará un elemento adelante (`top + 1`)
 
 	Args:
 		stack (list[int]): PILA  a la cual se intentará agregar el elemento
@@ -58,3 +113,11 @@ def push(stack: list[int], value: int, top: int) -> None:
 		top += 1
 		# Almacenar el nuevo valor en el tope (la cima de la PILA)
 		stack[top] = value
+
+# PILA que simula ser un arreglo estático
+#stack:list[int] = [10, 11, 12]
+# TOPE de la PILA
+#top:int = 2
+# "Eliminar" el elemento del tope de la PILA
+#wasRemoved, top, value = pop_multi_return(stack, top)
+#print(f'{wasRemoved=}\n{top=}\n{value=}\n{stack[top]=}')
--- classes/clase-27_02_2025/tests.py
+++ classes/clase-27_02_2025/tests.py
@@ -4,8 +4,8 @@ from queue import *
 
 class Tester(ut.TestCase):
     def test_stack_isEmpty(self):
-        self.assertFalse(isEmpty(0)) # True
-        self.assertTrue(isEmpty(1)) # True
+        self.assertTrue(isEmpty(0)) # True
+        self.assertFalse(isEmpty(1)) # True
     
     def test_stack_isFull(self):
         stack: list[int] = [0, 6, 23, 1]
@@ -20,6 +20,40 @@ class Tester(ut.TestCase):
         stack: list[int] = [0, 1, 2, None]
         push(stack, 12, 2)
         self.assertNotEqual(stack, [0, 1, 2, None]) # Debe ser verdadero
+    
+    def test_stack_pop_reference(self):
+        stack:list[int] = [10, 11, 12]
+        top_reference:list[int] = [2]
+        value_reference:list[int] = [None]
+        wasRemoved:int = pop_reference(stack, top_reference, value_reference)
+        self.assertEqual(wasRemoved, 1) # Debe ser cierto
+        self.assertEqual(value_reference[0], 12) # Debe ser cierto
+        # Simulando lista vacía (top = 0)
+        stack:list[int] = [0, 0, 0]
+        top_reference:list[int] = [0]
+        value_reference:list[int] = [None]
+        wasRemoved:int = pop_reference(stack, top_reference, value_reference)
+        self.assertEqual(wasRemoved, 0) # Debe ser cierto
+        self.assertEqual(value_reference[0], None) # Debe ser cierto
+
+    def test_stack_pop_multireturn(self):
+        stack:list[int] = [10, 11, 12]
+        top = 2
+        wasRemoved, top, value = pop_multi_return(stack, top)
+        # Se espera que las preposiciones sean verdaderas
+        self.assertEqual(wasRemoved, 1)
+        self.assertEqual(top, 1)
+        self.assertEqual(value, 12)
+        self.assertEqual(stack[top], 11)
+        # Cuando lista vacía
+        stack:list[int] = [0, 0, 0]
+        top = 0
+        wasRemoved, top, value = pop_multi_return(stack, top)
+        # Se espera que las preposiciones sean verdaderas
+        self.assertEqual(wasRemoved, 0)
+        self.assertEqual(top, 0)
+        self.assertEqual(value, None)
+        self.assertEqual(stack[top], 0)
 
 if __name__ == '__main__':
     ut.main(verbosity=2)
