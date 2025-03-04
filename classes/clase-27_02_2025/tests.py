560ec5d5c785a8087c4cb9d7971f581b9ff2ef39
tutosrive <tutosrivegamer@gmail.com>
2025-03-04 16:19:22 -0500

    Add tests try fixed

--- classes/clase-27_02_2025/tests.py
+++ classes/clase-27_02_2025/tests.py
@@ -4,7 +4,7 @@ from queue import *
 
 class Tester(ut.TestCase):
     def test_stack_isEmpty(self):
-        self.assertTrue(isEmpty(0)) # True
+        self.assertTrue(isEmpty(-1)) # True
         self.assertFalse(isEmpty(1)) # True
     
     def test_stack_isFull(self):
@@ -32,11 +32,12 @@ class Tester(ut.TestCase):
         self.assertEqual(value_reference[0], 12) # Debe ser cierto
         # Simulando lista vacía (top = 0)
         stack:list[int] = [0, 0, 0]
-        top_reference:list[int] = [0]
+        top_reference:list[int] = [-1]
         value_reference:list[int] = [None]
         wasRemoved:int = pop_reference(stack, top_reference, value_reference)
         self.assertEqual(wasRemoved, 0) # Debe ser cierto
         self.assertEqual(value_reference[0], None) # Debe ser cierto
+        self.assertEqual(top_reference[0], -1) # Debe ser cierto
 
     def test_stack_pop_multireturn(self):
         stack:list[int] = [10, 11, 12]
@@ -49,11 +50,11 @@ class Tester(ut.TestCase):
         self.assertEqual(stack[top], 11)
         # Cuando lista vacía
         stack:list[int] = [0, 0, 0]
-        top = 0
+        top = -1
         wasRemoved, top, value = pop_multi_return(stack, top)
         # Se espera que las preposiciones sean verdaderas
         self.assertEqual(wasRemoved, 0)
-        self.assertEqual(top, 0)
+        self.assertEqual(top, -1)
         self.assertEqual(value, None)
         self.assertEqual(stack[top], 0)
 
