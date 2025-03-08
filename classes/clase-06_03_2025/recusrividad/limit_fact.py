20baf718b12485edd3c27e9f70cc8e9dc1cce821
tutosrive <tutosrivegamer@gmail.com>
2025-03-08 15:41:45 -0500

    Update function to write the Esception in file .md

--- classes/clase-06_03_2025/recusrividad/limit_fact.py
+++ classes/clase-06_03_2025/recusrividad/limit_fact.py
@@ -4,9 +4,11 @@ file = open("./classes/clase-06_03_2025/recusrividad/data/stack_overflow.md", "a
 file.write("# Stack Overflow with FACTORIAL\n")
 n = 0
 # Ejecutar factorial hasta el desborde
-while True:
-    factorial = fact(n)
-    file.write(f"- X = {n}\n")
-    n += 1
-# Código muerto
+try:
+    while True:
+        factorial = fact(n)
+        file.write(f"- **X** = {n}\n")
+        n += 1
+except RecursionError as e:
+    file.write(f'\n-------------\n## Error\n {e.__class__.__name__} - {e}')
 file.close()
